c4d74f269623bb7a10ed43553d32978d5952589d
Al Green <a.l.green@hotmail.com>
2021-11-09 20:40:51 -0600

    add test get and post request

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ const app = express();
 app.listen(3000,function () {
     console.log('Server started. Listen on port 3000');
 })
-
-app.get('/api/products', (req, res) => {
-    const products = repoContext.products.findAllProducts();
-    return res.send(products);
+// tried with id 4 but only get the number 1
+app.get('/api/products/:id', (req, res) => {
+    const id = req.params.id;
+    const product = repoContext.products.findProductById(id);
+    return res.send(product);
 });
