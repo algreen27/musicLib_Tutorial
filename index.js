9405f5eb9f8c071f4388d29b456a89435c12112f
Al Green <a.l.green@hotmail.com>
2021-11-09 21:52:47 -0600

    add middleware folder, update index with get, post, put, and delete, add validation

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const repoContext = require('./repository/repository-wrapper');
+const cors =require('cors');
+const {validateProduct} = require('./middleware/product-validation');
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.listen(3000,function () {
     console.log('Server started. Listen on port 3000');
@@ -12,3 +18,22 @@ app.get('/api/products/:id', (req, res) => {
     const product = repoContext.products.findProductById(id);
     return res.send(product);
 });
+
+app.post('/api/products', [validateProduct], (req, res) => {
+    const newProduct = req.body;
+    const addedProduct = repoContext.products.createProduct(newProduct);
+    return res.send(addedProduct);
+});
+
+app.put('/api/products/:id', [validateProduct], (req, res) => {
+    const id = req.params.id;
+    const productPropertiesToUpdate = req.body;
+    const updatedProduct = repoContext.products.updateProduct(id, productPropertiesToUpdate)
+    return res.send(updatedProduct);
+});
+
+app.delete('/api/products/:id', (req, res) => {
+    const id = req.params.id;
+    const updatedDataSet = repoContext.products.deleteProduct(id);
+    return res.send(updatedDataSet);
+})
